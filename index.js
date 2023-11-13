ff84bd4c7bd82b70763857f3c0e324e6d334be6e
Márk Király <69594158+ProgrammerGnome@users.noreply.github.com>
2023-11-13 17:33:26 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,53 @@
+/**
+ * Fájl választó
+ */
+
 function toggleAccordion(fileNumber) {
     const fileContent = document.querySelector(`.file-item:nth-child(${fileNumber}) .file-content`);
     fileContent.style.display = (fileContent.style.display === 'block') ? 'none' : 'block';
+}
+
+
+/**
+ * Üdvözlő szöveg
+ */
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Szövegek, amiket felváltva szeretnénk gépelni
+    const textsToType = ["PROGRAMMER GNOME", "BACKEND DEVELOPER", "ARCH LINUX LOVER"];
+
+    // Időzítő, hogy megvárja a CSS animáció végezze el a gépelő hatást
+    setTimeout(() => {
+        startTyping(textsToType);
+    }, 1000);
+});
+
+function startTyping(texts) {
+    const typingContainer = document.getElementById('typing-container');
+    let currentTextIndex = 0;
+
+    function type() {
+        const text = texts[currentTextIndex];
+        let index = 0;
+
+        function typeCharacter() {
+            typingContainer.textContent += text[index];
+            index++;
+
+            if (index < text.length) {
+                setTimeout(typeCharacter, 100); // Az időzítés beállítja a karakterek közötti késleltetést
+            } else {
+                // Az egész szöveg kiírása után következő szövegre váltás
+                setTimeout(() => {
+                    typingContainer.textContent = ''; // Törlés a következő ismétlés előtt
+                    currentTextIndex = (currentTextIndex + 1) % texts.length; // Körkörös váltás a szövegek között
+                    type(); // Újraindítás a következő szöveggel
+                }, 1000);
+            }
+        }
+
+        typeCharacter();
+    }
+
+    type();
 }
